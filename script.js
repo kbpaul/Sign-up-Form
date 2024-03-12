2387150015e21e1d1071485eba25147b4759eedc
Paul Kum <kbpaul2016@gmail.com>
2024-03-13 01:41:54 +0400

    the Sign up page almost done. Though I am still working on how to make the input display in red when we click on the Create Account button without any input.

---
+++ script.js
@@ -0,0 +1,13 @@
+const inputElements = document.querySelectorAll('input');
+const button = document.querySelector('button');
+// button.addEventListener('click', addError);
+
+
+// // Goes throught the list of inputs and check if it's empty
+// // Add the error class to the input that is empty
+// function addError() {
+//     inputElements.forEach(input => {
+//         const inputValue = input.value;
+//         if()
+//     })
+// }
